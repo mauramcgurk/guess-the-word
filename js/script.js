f786b3cf555944ecb9ecc7951ad09c0bd0fcce5e
Maura McGurk <maura@mauramcgurk.com>
2022-10-13 16:59:13 -0700

    Continue to establish global variables

--- js/script.js
+++ js/script.js
@@ -1,2 +1,6 @@
 const guessedLetters = document.querySelector(".guessed-letters");
 const button = document.querySelector(".guess");
+const input = document.querySelector(".letter");
+//const wordInProgress = document.querySelector(".word-in-progress");
+//const remainingGuessesParagraph = document.querySelector(".remaining");
+//const remainingGuesses = document.querySelector(".remaining");
